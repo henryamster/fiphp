401ff39444c50215bc5ce364d9d6006cf54dcafc
Henry Fritz <henryamsterfritz@gmail.com>
2018-03-30 15:35:10 -0400

    added a few user queries, starting on other queries

--- includes/functions.php
+++ includes/functions.php
@@ -1,13 +1,65 @@
 <?php
-
+//Check for empty result field
 function confirm_query($result_set){
     if(!result_set){
         die("No results returned from database query.");
     }
 }
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//USERS functions
+
+//find ALL users, sorted by ID descending.
+function find_all_users(){
+    $query = "SELECT * ";
+    $query .= "FROM users";
+    $query .= "ORDER BY userID DESC";
+    $user_set = mysqli_query($link, $query);
+    confirm_query($user_set);
+}
+//find one user with name like $search string
+ function find_user(search){
+     $query = "SELECT * ";
+    $query .= "FROM users ";
+     $query .= "WHERE user like " . search . " ";
+     $user_set = mysqli_query($link, $query);
+     confirm_query($user_set);
+ }
+//return team members
+function find_team_members(stID){
+    $query = "SELECT * ";
+        $query .= "FROM users ";
+    $query .= "WHERE storeID = " . stID . " ";
+    $user_set = mysqli_query($link, $query);
+    confirm_query($user_set);
+}
+
+//adduser query
+function add_user(){
+    
+}
+//modify use query
+function modify_user(){
+    
+}
+//remove user query
+function remove_user(){
+    
+}
+
+//ITEMS function
+function find_all_items(){
+    $query = "SELECT * ";
+    $query .= "FROM item";
+    $query .= "ORDER BY itemID DESC";
+    $item_set = mysqli_query($link, $query);
+    confirm_query($item_set);
+}
+
+ function find_item(search){
+     $query = "SELECT *";
+    $query .= "FROM item";
+     $query .= "WHERE itemName like " . search . " ";
+     $item_set = mysqli_query($link, $query);
+     confirm_query($item_set);
+ }
+
 
